b4d6249175ddd35b8966d2762022b3f627d24f89
Sergiu Waxmann <waxmann.sergiu@me.com>
2020-08-10 13:28:20 +0300

    Initial commit

---
+++ android/app/src/main/java/com/sergiuwaxmann/courseapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.sergiuwaxmann.courseapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CourseApp());
+
+class CourseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+  }
+}
